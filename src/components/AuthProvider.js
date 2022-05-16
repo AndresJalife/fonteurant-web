d6a2b1391aebfc2397555eb6a058fa7991456235
Maximiliano Levi <maxilevi77@gmail.com>
2022-05-16 01:23:13 -0300

    Create auth provider to handle authentication using context

---
+++ src/components/AuthProvider.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import {Navigate, useLocation} from "react-router";
+import ApiRoutes from "../ApiRoutes";
+
+let AuthContext = React.createContext(null);
+
+export default function AuthProvider({ children }) {
+    let [user, setUser] = React.useState(null);
+
+    let signIn = async (email, password) => {
+        let response = await ApiRoutes.signIn(email, password);
+        console.log(response);
+        let token = response['Authorization'];
+        if (!token) {
+            console.log('LogIn failed.');
+            return {error: true};
+        }
+        localStorage.setItem('token', token);
+        response = await ApiRoutes.profile();
+        console.log(response);
+        setUser(response);
+        return {};
+    };
+
+    let signOut = (callback) => {
+        localStorage.removeItem('token');
+        setUser(null);
+        callback();
+    };
+
+    let value = { user, signIn, signOut };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+    return React.useContext(AuthContext);
+}
