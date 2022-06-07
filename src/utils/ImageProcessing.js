3877b882e12ebc45ad9dcfd0035a734659d6064e
Maximiliano Levi <maxilevi77@gmail.com>
2022-06-07 00:13:32 -0300

    crop images to square res

--- src/utils/ImageProcessing.js
+++ src/utils/ImageProcessing.js
@@ -1,4 +1,4 @@
-export const processImage = async (file) => {
+export const processImage = async (file, width, height) => {
     const getBase64 = (file) => new Promise((resolve, reject) => {
         let reader = new FileReader();
         reader.readAsDataURL(file);
@@ -10,11 +10,45 @@ export const processImage = async (file) => {
         };
     });
 
-    // Deberiamos recortar la foto?
+    const data = await getBase64(file);
+    return await cropImage(data);
 
-    return await getBase64(file);
 };
 
+const cropImage = async (dataUrl) => {
+    const originalImage = new Image();
+    originalImage.src = dataUrl;
+
+    const cropJob = new Promise((resolve, reject) => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        originalImage.addEventListener('load', function() {
+
+            const minDim = Math.min(originalImage.width, originalImage.height);
+            canvas.width = minDim;
+            canvas.height = minDim;
+
+            //draw the image
+            ctx.drawImage(
+                originalImage,
+                (originalImage.width - minDim) / 2,
+                (originalImage.height - minDim) / 2,
+                minDim,
+                minDim,
+                0,
+                0,
+                minDim,
+                minDim
+            );
+
+            resolve(canvas.toDataURL("image/jpeg", 0.9));
+        });
+    })
+
+    return await cropJob;
+}
+
 export const isValidImage = (file) => {
-    return file.size / 1024 <= 512;
+    return file.size / 1024 <= 1024;
 };
